fe6a5ecc3c3b1f1e055475760ae0bca539261ed5
Ruth <ruthasafuadjaye@yahoo.com>
2025-01-23 21:47:28 +0200

    Fixed search functionality

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Rating, Stack, Typography } from "@mui/material";
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Rating, Stack, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Product } from "../types/product";
 import { getProducts } from "../services/api";
@@ -7,11 +7,35 @@ import { Link } from "react-router-dom";
 
 function Products() {
   const [products, setProducts] = useState<Product[]>([]);
-
+  
   //search bar
+  const [searchInput, setSearchInput] = useState<string>('');
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+
+  const handleSearch = (event: any) => {
+    event.preventDefault();
+
+      const searchInput = event.target.value.toLowerCase();
+      setSearchInput(searchInput);
+      console.log(searchInput);
+  
+      if (searchInput === '') {
+        setFilteredProducts(products);
+      } else {
+        const filteredProducts = products.filter((product) =>
+          product.title.toLowerCase().includes(searchInput)
+        );
+        setFilteredProducts(filteredProducts);
+      }
+    };
+
 
   useEffect(() => {
-    getProducts().then((products) => setProducts(products)); }, []);
+      getProducts().then((products) => {
+        setProducts(products);
+        setFilteredProducts(products); // Show all products initially
+      });
+    }, []);
 
   console.log(products);
 
@@ -20,12 +44,16 @@ function Products() {
     <div>
     <Typography variant="h4" component="h1" gutterBottom> Products Page </Typography>
 
+    <TextField type="text" value={searchInput} label="search" onChange={handleSearch}></TextField>
+    
+
     <Box sx ={{mx:'auto', p:2}}>
       <div>
-      <Stack sx={{justifyContent: 'space-around', direction:'row', flexWrap: 'wrap', display: 'grid', gap: 5}}>
+      <Stack display='row' sx={{justifyContent: 'space-around', flexWrap: 'wrap',  gap: 5}}>
 
-       {products.map ((product) => (
-        <Card sx = {{minWidth: 200, display:"flex", flexDirection:"column", justifyContent:"space-between", width: '15%'}}>
+       {filteredProducts.map ((product) => (
+        <Card sx = {{minWidth: 200, display:"flex", flexDirection:"column", justifyContent:"space-between", width: '15%'}}
+              key= {product.id}>
           <CardMedia sx = {{height:250}} image={product.image} title={product.title} ></CardMedia>
             <Typography variant="h6" component="h2">{product.title}</Typography>
             <Typography variant="body2" component="p">{product.price}</Typography>
